19bc105deecc6e3c2c288411d770271509b3fe7d
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-05-11 12:25:27 +0600

    new user data getting from client side

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const users = [
   {
@@ -33,7 +34,12 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
+  console.log("post api hitting");
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.json(users);
 });
 
 app.listen(port, () => {
